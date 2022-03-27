c76c5cc56464ad2062a166cbe7bd91a2a3a6aba7
Isai Rodriguez <macover1507@hotmail.com>
2022-03-26 21:07:18 -0600

    segunda parte

--- PerceptronMulticapa/Form1.Designer.cs
+++ PerceptronMulticapa/Form1.Designer.cs
@@ -29,13 +29,34 @@ namespace PerceptronMulticapa
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnEmpezar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnEmpezar
+            // 
+            this.btnEmpezar.Location = new System.Drawing.Point(126, 47);
+            this.btnEmpezar.Name = "btnEmpezar";
+            this.btnEmpezar.Size = new System.Drawing.Size(75, 23);
+            this.btnEmpezar.TabIndex = 0;
+            this.btnEmpezar.Text = "Entrenar";
+            this.btnEmpezar.UseVisualStyleBackColor = true;
+            this.btnEmpezar.Click += new System.EventHandler(this.btnEmpezar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(329, 267);
+            this.Controls.Add(this.btnEmpezar);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnEmpezar;
     }
 }
 
--- PerceptronMulticapa/Form1.cs
+++ PerceptronMulticapa/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,73 @@ namespace PerceptronMulticapa
 {
     public partial class Form1 : Form
     {
+
+        int[] arquitecturaRed;
+        double[,] patronesEntrada;
+        double[,] patronesSalida;
+
+        int numeroCapas = 0, neuronasEntrada=0, neuronasSalida=0, numeroPatrones=0;
+        Int32 iteracionesMaximas = 0;
+        int[] n;
+        double alfa = 0.0, errorMinimo = 0.0, neuronasMaximas = 0.0;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnEmpezar_Click(object sender, EventArgs e)
+        {
+            leerTxt();
+        }
+
+        private void leerTxt()
+        {
+            string[] datos;
+            int numeroLinea = 0, posI = 0, posJ = 0;
+            
+            StreamReader sr = new StreamReader("grafica.txt");
+            string linea = sr.ReadLine();
+            int posicionSalida = 0;
+
+            while (linea != null)
+            {
+                switch (numeroLinea)
+                {
+                    case 0:
+                        datos = linea.Split(' ');
+                        numeroCapas = Convert.ToInt16(datos[0]);
+                        arquitecturaRed = new int[Convert.ToInt16(datos[0]) + 1];
+                        for (int i = 0; i < arquitecturaRed.Length; i++)
+                        {
+                            arquitecturaRed[i] = Convert.ToInt16(datos[i]);
+                        }
+                        n = new int[numeroCapas + 1];
+                        n = arquitecturaRed;
+                        break;
+                    case 1:
+                        alfa = Convert.ToDouble(linea);
+                        break;
+                    case 2:
+                        errorMinimo = Convert.ToDouble(linea);
+                        break;
+                    case 3:
+                        iteracionesMaximas= Convert.ToInt32(linea);
+                        break;
+                    case 4:
+                        numeroPatrones = Convert.ToInt16(linea);
+                        break;
+                }
+                if(numeroLinea == 4)
+                {
+                    patronesEntrada = new double[numeroPatrones + 1, arquitecturaRed[1]];
+                    patronesSalida = new double[numeroPatrones + 1, arquitecturaRed[1]];
+                }
+                if(numeroLinea <= (numeroPatrones + 4) && numeroLinea > 4)
+                {
+                    //min6.45
+                }
+            }
         }
     }
 }
