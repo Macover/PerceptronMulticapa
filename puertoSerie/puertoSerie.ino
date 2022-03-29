b9bcbd47478e4525433b77ef69822494ae6b2b86
Isai Rodriguez <macover1507@hotmail.com>
2022-03-29 10:06:32 -0600

    .ino añadido

---
+++ puertoSerie/puertoSerie.ino
@@ -0,0 +1,24 @@
+#include <ArduinoJson.h>
+#include <analogWrite.h>
+//Variables
+int trigger = 5;
+int echo = 18;
+float tiempo, distancia;
+
+void setup() {
+  pinMode(trigger, OUTPUT);
+  pinMode(echo, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:    
+    digitalWrite(trigger, LOW);
+    delayMicroseconds(2);
+    digitalWrite(trigger, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigger, LOW);
+    tiempo = pulseIn (echo, HIGH);
+    distancia = (tiempo / 2) / 29.15;
+    Serial.println(distancia);
+}
